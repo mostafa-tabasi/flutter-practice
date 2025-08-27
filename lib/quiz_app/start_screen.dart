00b344cfffbcb0c5db3ba23e556091e051441edf
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-08-27 19:41:00 +0330

    (Quiz App) changes:
    - Added an icon to the button in start screen.
    - Made the picture slightly transparent.

--- lib/quiz_app/start_screen.dart
+++ lib/quiz_app/start_screen.dart
@@ -9,17 +9,22 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset("assets/images/quiz-logo.png", width: 200),
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 200,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
           SizedBox(height: 40),
           Text(
             "Learn Flutter the fun way!",
             style: TextStyle(fontSize: 28, color: Colors.white),
           ),
           SizedBox(height: 40),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: Text("Start quiz", style: TextStyle(fontSize: 20)),
+            icon: Icon(Icons.arrow_right_alt),
+            label: Text("Start quiz", style: TextStyle(fontSize: 20)),
           ),
         ],
       ),
