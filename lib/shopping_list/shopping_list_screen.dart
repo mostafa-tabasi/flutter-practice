9a38bd2fe8a4d86c031aa8faa1c38e3064967934
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-09-17 17:44:35 +0330

    (Shopping List App) Made list items dismissable

--- lib/shopping_list/shopping_list_screen.dart
+++ lib/shopping_list/shopping_list_screen.dart
@@ -26,25 +26,47 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) {
+          final item = _groceryItems[index];
+
+          return Dismissible(
+            key: ValueKey(item.id),
+            onDismissed: (direction) {
+              _removeItem(item);
+            },
+            child: ListTile(
+              title: Text(item.name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: item.category.color,
+              ),
+              trailing: Text(item.quantity.toString()),
+            ),
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addNewItem, icon: Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
-        ),
-      ),
+      body: content,
     );
   }
 }
