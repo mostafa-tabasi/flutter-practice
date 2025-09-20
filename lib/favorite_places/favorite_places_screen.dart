c11ad22c846fa645b7d43996ad5e492a3623748f
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-09-20 09:45:57 +0330

    (Favorite Places App) Initiated the app

---
+++ lib/favorite_places/favorite_places_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritePlacesScreen extends StatelessWidget {
+  const FavoritePlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Favorite Places'));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/dice_roller/dice_roller_screen.dart';
 import 'package:flutter_practice/expense_tracker/expenses_screen.dart';
+import 'package:flutter_practice/favorite_places/favorite_places_app.dart';
+import 'package:flutter_practice/favorite_places/favorite_places_screen.dart';
 import 'package:flutter_practice/meals/meals_app.dart';
 import 'package:flutter_practice/quiz/quiz_screen.dart';
 import 'package:flutter_practice/shopping_list/shopping_list_app.dart';
@@ -55,6 +57,7 @@ class HomeScreen extends StatelessWidget {
           NavigatorButton("Expense Tracker", const ExpensesScreen()),
           NavigatorButton("Meals App", ProviderScope(child: const MealsApp())),
           NavigatorButton("Shopping List", const ShoppingListApp()),
+          NavigatorButton("Favorite Places", const FavoritePlacesApp()),
         ],
       ),
     );
