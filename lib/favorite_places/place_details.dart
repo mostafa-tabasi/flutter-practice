490dd7d74e871ab608f7fbde71b1796ede989b25
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-09-21 11:51:34 +0330

    (Favorite Places App) Displayed the place image in place details screen

--- lib/favorite_places/place_details.dart
+++ lib/favorite_places/place_details.dart
@@ -10,11 +10,15 @@ class PlaceDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(place.title)),
-      body: Center(
-        child: Text(
-          place.title,
-          style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-        ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
       ),
     );
   }
