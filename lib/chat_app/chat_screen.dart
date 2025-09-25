fe749671e324c5363329635a661fe7842e3e8f23
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-09-25 20:51:13 +0330

    (Chat App) Handled sending message functionality

--- lib/chat_app/chat_screen.dart
+++ lib/chat_app/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/chat_app/widgets/chat_messages.dart';
+import 'package:flutter_practice/chat_app/widgets/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -18,7 +20,12 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(child: Text('Logged In!')),
+      body: Column(
+        children: const [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/chat_app/widgets/chat_messages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('no message yet'));
+  }
+}
