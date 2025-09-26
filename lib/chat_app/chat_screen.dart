a87f118a5e05c3e9b0b0ec771a9d94b6e1ab3554
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2025-09-26 13:54:31 +0330

    (Chat App) Initialized cloud messaging

--- lib/chat_app/chat_screen.dart
+++ lib/chat_app/chat_screen.dart
@@ -1,16 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/chat_app/widgets/chat_messages.dart';
 import 'package:flutter_practice/chat_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void _setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+    print('TOKEN = $token');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Chat App'),
+        title: Text('Flutter Chat'),
         actions: [
           IconButton(
             onPressed: () {
